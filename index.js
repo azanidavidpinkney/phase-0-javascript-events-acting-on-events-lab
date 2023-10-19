2a84cdcb1152dd72b55536d395c5055f171549b8
Azani David Pinkney <info@azani.org>
2023-10-18 22:53:15 -0700

    movement lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+// Changes our language from HTML to DOM
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    // Check if moving to the right would keep the dodger within the container width
+    if (left < 360) { 
+      dodger.style.left = `${left + 1}px`; // Move the dodger to the right by 1 pixel
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") { // Check for the right arrow key
+      moveDodgerRight();
+    }
+});
